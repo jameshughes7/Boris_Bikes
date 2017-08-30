921ea9c28449248a3e7906459db6fcf87ff522bf
James Hughes <hugjimbo77@gmail.com>
2017-08-30 15:09:47 +0100

    added release_bike to docking_station file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,11 @@
-require 'docking_station'
-describe DockingStation do
+#impicitly defined subject syntax
 
+require 'docking_station'
+
+describe DockingStation do
+  it {is_expected.to respond_to :release_bike}
 end
+
+#Given DockingStation
+#When :release_bike is called on instance of DockingStation
+#Then respond (respond_to release_bike method with true or false)
